edc8a6d4a30faa44903f7fae8165d5ea3e04e820
wangyangyang <769789170@qq.com>
2019-09-23 22:12:14 +0800

    打印sql

---
+++ src/main/java/com/riverluoo/configuration/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.riverluoo.configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @auther: wangyang
+ * @since: 2019-09-23 22:08
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+
+
+}
